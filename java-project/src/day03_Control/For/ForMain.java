142a60b66f2a4a76c95d9a564c939db04446c897
CoffeerLatte <125641153+CoffeerLatte@users.noreply.github.com>
2023-11-22 17:53:18 +0900

    updating

---
+++ java-project/src/day03_Control/For/ForMain.java
@@ -0,0 +1,116 @@
+package day03_Control.For;
+
+import java.util.*;
+
+public class ForMain {
+
+		public static void main(String[] args) {
+			
+		try {
+		int one, two;
+		String three;
+		System.out.println("숫자 2개와 연산자를 입력해주세요.");
+		
+		one = new Scanner(System.in).nextInt();
+		two = new Scanner(System.in).nextInt();
+		three = new Scanner(System.in).next();
+		
+		
+		
+		switch(three)
+		{
+		case "*" :
+			{
+				System.out.println(one*two);
+				break;
+			}
+		case "+" :
+		{
+			System.out.println(one+two);
+			break;
+		}
+		case "-" :
+		{
+			System.out.println(one-two);
+			break;
+		}
+		case "/" :
+		{
+			System.out.println(one/two);
+			break;
+		}
+			
+		}
+		
+		}
+
+		catch (Exception e) {
+			System.out.println("Exception ");
+			System.out.println("0으로 나눌 수 없습니다.");
+		}
+
+		}
+
+}
+//switch(j) {
+//case 1:
+//{
+//	System.out.print("*"+"\n");
+//	break;
+//}
+//case 2:
+//{
+//	System.out.print("**"+"\n");
+//	break;
+//}
+//case 3:
+//{
+//	System.out.print("***"+"\n");
+//	break;
+//}
+//case 4:
+//{
+//	System.out.print("****"+"\n");
+//	break;
+//}
+//case 5:
+//{
+//	System.out.print("*****"+"\n");
+//	break;
+//}
+			
+
+
+// 별 1번
+//package day03_Control.For;
+
+//import java.util.*;
+//
+//public class ForMain {
+//for(int i=1; i <=5; i++)
+//{
+//	
+//	for(int j =1; j <=i; j++)
+//	{
+//		System.out.print("*");
+//	}
+//		System.out.println();
+//}
+//}
+
+//별 2번
+// package day03_Control.For;
+
+//import java.util.*;
+//
+//public class ForMain {
+//for(int i=5; i >=1; i--)
+//{
+//	
+//	for(int j =1; j <= i; j++)
+//	{
+//		System.out.print("*");
+//	}
+//		System.out.println();
+//}
+//}
--- java-project/src/day03_Control/For/QuizEx02.java
+++ java-project/src/day03_Control/For/QuizEx02.java
@@ -1,17 +1,34 @@
+// 문제 1: 1부터 100까지의 합.
+
+//package day03_Control.For;
+//
+//
+//public class QuizEx02 {
+//	public static void main(String[] args) {
+//		
+//		
+//		int i, sum=0;
+//		
+//		for (i=1; i<=100; i ++)
+//		{
+//			sum += i;
+//		}
+//		
+//		System.out.println("1부터 100까지 합은: "+sum);
+//	}
+//}
+
+
+
+//문제 2
 package day03_Control.For;
 
-
-public class QuizEx02 {
+public class QuizEx02{
+	
 	public static void main(String[] args) {
 		
 		
-		int i, sum=0;
 		
-		for (i=1; i<=100; i ++)
-		{
-			sum += i;
-		}
 		
-		System.out.println("1부터 100까지 합은: "+sum);
 	}
 }
