06e1406cbc7d1593c94b2c9dcf2e74797f3c9381
CoffeerLatte <125641153+CoffeerLatte@users.noreply.github.com>
2023-11-23 17:54:52 +0900

    sdads

---
+++ java-project/src/day04_homework/Star.java
@@ -0,0 +1,25 @@
+package day04_homework;
+
+public class Star {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println();
+
+			for (int j = 0; j < i; j++) {
+
+				 System.out.print("*");
+
+				
+
+				continue ;
+				
+			}
+
+		} // for2 end
+	}
+
+}
+		
+
