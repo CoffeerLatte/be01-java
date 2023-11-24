be1f2c2695aea4ed15e6b0dc6d62bca00a42d77d
CoffeerLatte <125641153+CoffeerLatte@users.noreply.github.com>
2023-11-24 17:50:00 +0900

    updating

--- java-project/src/day05_random/Lotto.java
+++ java-project/src/day05_random/Lotto.java
@@ -3,17 +3,33 @@ package day05_random;
 public class Lotto {
 
 	public static void main(String[] args) {
-		int a = (int)((Math.random()*45));
-		
-		int a2=(int)((Math.random()*45));
-				int  a3= (int)((Math.random()*45));
-				int a4= (int)((Math.random()*45));
-				int a5 = (int)((Math.random()*45));
-				int a6 =(int)((Math.random()*45));
-	
-		
+
+		int a2,a3,a4,a5,a6;
 				
-		System.out.println(a);
+	
+		while(true) {
+			int a1 = (int)(Math.random()*45);
+			System.out.println(a1);
+			
+			if(a1 == (int)(Math.random()*45))
+			{
+				for(;a1!=(int)(Math.random()*45);)
+				{
+					a2 = (int)(Math.random()*45);
+				}
+			}
+			else if(a2 == (int)(Math.random()*45)) {
+				for(;a2!=(int)(Math.random()*45);)
+				{
+					a3=(int)(Math.random()*45)
+					if
+					a2 = (int)(Math.random()*45);
+				}
+				
+			}
+			
+		}
+		
 		System.out.println( a==a2 ? (int)(Math.random()*45) : a2 );
 		
 		
